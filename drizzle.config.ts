ffd1787048e04e6e89c819e69e5173b5be300103
Karlo-DEV19 <Karlomangampo392@gmail.com>
2026-04-22 20:30:30 +0800

    Add files via upload

---
+++ drizzle.config.ts
@@ -0,0 +1,29 @@
+// drizzle.config.ts
+import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
+import path from "path";
+
+// Load environment variables
+config({ path: path.resolve(__dirname, ".env.local") });
+
+export default defineConfig({
+    schema: [
+        "./src/schema/admin/admins.ts",
+        "./src/schema/products/blinds/blinds-product.ts",
+        "./src/schema/products/blinds/blinds-product-image.ts",
+        "./src/schema/products/blinds/blinds-product-colors.ts",
+        "./src/schema/products/blinds/blinds-product-relations.ts",
+        "./src/schema/otp/otp-code.ts",
+        "./src/schema/orders/orders.ts",
+        "./src/schema/orders/payment-history/payment-history.ts",
+        "./src/schema/activity-logs/activity-logs.ts",
+        "./src/schema/notifications/notifications.schema.ts"
+    ],
+    out: "./drizzle/migrations",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL ?? "",
+    },
+    verbose: true,
+    strict: true,
+});
---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgs.search.brave.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
